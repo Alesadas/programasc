3030d22884abaebaac45cab33adcd36dd78f2782
Alesadas <david.salazar.defaz@udla.edu.ec>
2023-05-02 12:58:21 -0500

    commit2

--- Deberes/examenp1/examenp1.c
+++ Deberes/examenp1/examenp1.c
@@ -2,8 +2,27 @@
 #include<stdio.h>
 int main (){
     //identificacion de variables int por que es entero positivo
-    int numero, n;
-    printf("ingrese le numero a determinar:\n");}
-    scanf("%d",&numero);
+    int x, n;
+    int hexa;
+    x=1;
+    hexa=<>;
+    printf("ingrese le numero a determinar:\n");
+    scanf("%d",&n);
+    //utilizamos un ciclo repetitvo mientras x sea mayor que cero
+    while (x>0)
+    {
+        x=trunc(n/16);
+        n%16=0;
+        hexa=<A>+hexa;
+        hexa=<B>+hexa;
+        hexa=<C>+hexa;
+        hexa=<D>+hexa;
+        hexa=<E>+hexa;
+        hexa=<F>+hexa;
+        hexa=(n%16)+hexa;
+        n=x;
+       
+    }
     
+
 }
