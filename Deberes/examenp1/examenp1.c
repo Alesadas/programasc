74cc3439cb59b6e7e2e346514c9d13e6df33b4b7
Alesadas <david.salazar.defaz@udla.edu.ec>
2023-05-02 12:29:04 -0500

    commit1

---
+++ Deberes/examenp1/examenp1.c
@@ -0,0 +1,9 @@
+//Construya un programa que reciba como dato de entrada un número entero positivo, n, y regrese como dato de salida la representación de este número en hexadecimal.
+#include<stdio.h>
+int main (){
+    //identificacion de variables int por que es entero positivo
+    int numero, n;
+    printf("ingrese le numero a determinar:\n");}
+    scanf("%d",&numero);
+    
+}
