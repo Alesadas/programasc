a0727d4c2dc785b5a1778494afd3f3e42bb8aa09
Alesadas <david.salazar.defaz@udla.edu.ec>
2023-05-18 12:07:22 -0500

    commit1

---
+++ Deberes/examenp1/ejerciciosclase/matrizcualquiera.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    //defino las variables y el tamaño de la matriz
+    int numeros[n][m],filas, columnas;
+    printf("digite el numero de filas:\n");
+    scanf("%d",&filas);
+    printf("digite el numero de columnas:\n");
+    scanf("%d",&columnas);
+
+}
