40383608fbd88e48eb36b57cb9fbf1834816920f
Alesadas <david.salazar.defaz@udla.edu.ec>
2023-05-23 13:06:36 -0500

    transpuestacommit2

--- Deberes/examenp1/ejerciciosclase/semana9/matriztranspuesta.c
+++ Deberes/examenp1/ejerciciosclase/semana9/matriztranspuesta.c
@@ -26,3 +26,25 @@ void calcularTranspuesta(int **matriz, int **transpuesta, int filas, int columna
     }
 } 
 
+int main() {
+    int filas, columnas;
+
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+
+    // Crear la matriz
+    int **matriz = (int **)malloc(filas * sizeof(int *));
+    for (int i = 0; i < filas; i++) {
+        matriz[i] = (int *)malloc(columnas * sizeof(int));
+    }
+
+    // Llenar la matriz con números aleatorios entre 0 y 100
+    srand(time(NULL));
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = generarAleatorio(0, 100);
+        }
+    }
